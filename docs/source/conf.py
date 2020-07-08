ad55ac7728f73f432e0a23c097fe1b1517fd7533
Nick Konz <nickkonz3@gmail.com>
2020-07-08 15:04:28 -0400

    2.4.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Nick Konz'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'2.4.6'
+release = u'2.4.7'
 
 
 # -- General configuration ---------------------------------------------------
